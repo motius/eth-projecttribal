febdfdf7561b85840bb95f1d3dc6c921e9700bc2
Nikolay Dimolarov <dimolarov@hotmail.com>
2018-01-20 19:38:05 +0100

    added simple web client for testing stuff

---
+++ ether-client/js/contracts.js
@@ -0,0 +1,13 @@
+web3 = new Web3(new Web3.providers.HttpProvider("http://192.168.1.113:8545"));
+abi =
+JSON.parse('[{"constant":false,"inputs":[{"name":"_userId","type":"address"}],"name":"makeUserAdmin","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getName","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_userId","type":"address"}],"name":"makeFanAUser","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"newUser","type":"address"}],"name":"addUser","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"_userId","type":"address"}],"name":"getUser","outputs":[{"name":"","type":"address"},{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"getNumberOfMembers","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_userId","type":"address"}],"name":"makeUserAFan","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_userId","type":"address"}],"name":"makeAdminUser","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[{"name":"_name","type":"string"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"}]')
+
+
+
+fanClubContract = web3.eth.contract(abi);
+contractInstance = fanClubContract.at('0x11247e95a07abce6c40e125f55e38e10d4a99d94');
+
+function getNumberOfMembers() {
+  console.log(contractInstance)
+  console.log(contractInstance.getNumberOfMembers())
+}
