420ee89f742623dc9370d09ba59eb205af6f0e48
Dominic Giebert <dominic.giebert@motius.de>
2018-01-21 00:11:41 +0100

    changed FanClub adding members and deploy scripts for rpc

--- ether/deploy.js
+++ ether/deploy.js
@@ -17,14 +17,15 @@ const filename = 'FanClub-AbiDefinition.js'
 fs.writeFile('abi/' + filename, JSON.stringify(abiDefinition), 'utf8') // String to harddisk
 console.log("Stored ABI in: " + filename)
 
-// Create the contract from ABI Definitions
-const fanClubContract = web3.eth.contract(abiDefinition)
-
 // Extract the byte code out of the json
 const byteCode = compiledCode.contracts[':FanClub'].bytecode
 
 // Unlock the first account
+web3.eth.defaultAccount = web3.eth.accounts[0]
 web3.personal.unlockAccount(web3.eth.accounts[0], "lorenzo")
+
+// Create the contract from ABI Definitions
+const fanClubContract = web3.eth.contract(abiDefinition)
 
 // Create the contract with an admin user (lorenzo)
 let deployedContract = fanClubContract.new('Lorenzo',{data: '0x' + byteCode, from: web3.eth.accounts[0], gas: 9000000}, function(error, result){
