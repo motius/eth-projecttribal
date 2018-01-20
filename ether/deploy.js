85b8b6da7e529eecf2c38ffa48bea4dbe45ff675
Dominic Giebert <dominic.giebert@motius.de>
2018-01-20 19:05:10 +0100

    Adding deploy script and generation of blockchain

---
+++ ether/deploy.js
@@ -0,0 +1,38 @@
+const Web3 = require('web3')
+const solc = require('solc')
+const fs = require('fs');
+
+// Connect to a node using the RPC protocol (web3.js)
+const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"))
+
+// Get the raw contract (read from fs)
+const code = fs.readFileSync('../truffle/contracts/FanClub.sol').toString()
+
+// compile the shitty code
+const compiledCode = solc.compile(code)
+
+// Extract the Definition of functions
+const abiDefinition = JSON.parse(compiledCode.contracts[':FanClub'].interface)
+const filename = 'FanClub-AbiDefinition.js'
+fs.writeFile('abi/' + filename, JSON.stringify(abiDefinition), 'utf8') // String to harddisk
+console.log("Stored ABI in: " + filename)
+
+// Create the contract from ABI Definitions
+const fanClubContract = web3.eth.contract(abiDefinition)
+
+// Extract the byte code out of the json
+const byteCode = compiledCode.contracts[':FanClub'].bytecode
+
+// Unlock the first account
+web3.personal.unlockAccount(web3.eth.accounts[0], "lorenzo")
+
+// Create the contract with an admin user (lorenzo)
+let deployedContract = fanClubContract.new('Lorenzo',{data: '0x' + byteCode, from: web3.eth.accounts[0], gas: 9000000}, function(error, result){
+    if(!error){
+        const contractAddress = result.address
+        console.log("Contract Address:" + contractAddress)
+        web3.personal.lockAccount(web3.eth.accounts[0])
+    } else {
+        console.log(error)
+    }
+})
---
+++ ether/generate.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+rm -Rf testing
+
+mkdir -p testing/1
+mkdir -p testing/2
+mkdir -p testing/3
+
+geth --datadir testing/1/data init ./genesis.json
+geth --datadir testing/2/data init ./genesis.json
+geth --datadir testing/3/data init ./genesis.json
+
+cp keys/nikolay.json keys/lorenzo.json testing/1/data/keystore
+cp keys/nikolay.json keys/lorenzo.json testing/2/data/keystore
+cp keys/nikolay.json keys/lorenzo.json testing/3/data/keystore
+
+tilix -a session-add-right -e "bootnode --nodekey=./boot.key -verbosity 9 -nat any"
+
+tilix -a session-add-down -e "geth --datadir testing/2/data --networkid 1900 --bootnodes enode://dcb403ce5241b7454a5fa2ba52c7def7df7759b720f74036ead066ac56e3aa4e3be40f678347a5a9c1bd1b4d3a26b9a0823111392f87e5606c125126916b9a6d@127.0.0.1:30301 --port 30304 console"
+tilix -a session-add-down -e "geth --datadir testing/3/data --networkid 1900 --bootnodes enode://dcb403ce5241b7454a5fa2ba52c7def7df7759b720f74036ead066ac56e3aa4e3be40f678347a5a9c1bd1b4d3a26b9a0823111392f87e5606c125126916b9a6d@127.0.0.1:30301 --port 30305 console"
+geth --datadir testing/1/data --networkid 1900 --bootnodes enode://dcb403ce5241b7454a5fa2ba52c7def7df7759b720f74036ead066ac56e3aa4e3be40f678347a5a9c1bd1b4d3a26b9a0823111392f87e5606c125126916b9a6d@127.0.0.1:30301 --port 30303 console --rpc --rpcaddr 0.0.0.0 --rpcapi "db,eth,net,web3,personal"
